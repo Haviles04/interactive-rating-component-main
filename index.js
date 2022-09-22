a6c33a14905e556ebae975c0197357acd4337cfe
Haviles04 <108551827+Haviles04@users.noreply.github.com>
2022-09-22 10:35:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,19 @@
 
 
    const ratingForm = document.getElementById('ratingForm');
-   const ratingAnswer = document.getElementsByName('rating:checked');
+   ;
    const ratingQuestion = document.getElementById('ratingQuestion');
    const thankYouBox = document.getElementById('thankYou');
    const ratingSelect = document.getElementById('chosenRating');
-   const answer = ratingAnswer.value;
+   
 
    
  
 
  const ratingSheetAnswer = () => {
+ const ratingAnswer = document.getElementsByName('rating:checked')
+ const answer = ratingAnswer.value;
+         
          ratingQuestion.style.visibility ="hidden";
          thankYouBox.style.visibility ="visible";
          ratingSelect.innerHTML=`You selected ${answer} out of 5`
