8107aebba46b3064e30a0b83cebc90b206e14e0e
Haviles04 <108551827+Haviles04@users.noreply.github.com>
2022-09-21 21:18:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 
 
 
-   const ratingForm = document.getElementById('ratingForm');
-   const ratingAnswer = document.getElementsByName('rating:checked');
+   
+   const answer = document.querySelector('.rating:checked').value;
    const ratingQuestion = document.getElementById('ratingQuestion');
    const thankYouBox = document.getElementById('thankYou');
    const ratingSelect = document.getElementById('chosenRating');
-   const answer = ratingAnswer;
+   
 
    
  
