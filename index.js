6c7f8c5c8d324b2289bae1fbddc2322dd277dc7c
Haviles04 <Haviles04@gmail.com>
2022-09-21 20:17:11 -0400

    Changed function back to original idea(not working)

--- index.js
+++ index.js
@@ -4,19 +4,14 @@
 
 
 
-const ratingSheetAnswer = () => {
-   const ratingAnswer = document.getElementsByName('rating');
+   const ratingAnswer = document.getElementsByName('rating:checked').value;
    const ratingQuestion = document.getElementById('ratingQuestion');
    const thankYouBox = document.getElementById('thankYou');
    const ratingSelect = document.getElementById('chosenRating');
 
-   for(let i = 0; i < ratingAnswer.length; i ++){
-      if (ratingAnswer[i].checked){
+ const ratingSheetAnswer = () => {
          ratingQuestion.style.visibility ="hidden";
          thankYouBox.style.visibility ="";
          ratingSelect.innerHTML="You selected ${ratingAnswer} out of 5"
-      } else {
-         ratingSelect.innerHTML="Error, please try again!"
-      }
-   }
-};
+      
+   };
