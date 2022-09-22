6a2f5fb57d11aef4c92d729dd297f2fa320bc946
Haviles04 <Haviles04@gmail.com>
2022-09-21 20:49:00 -0400

    Worked out javascript running.

--- index.js
+++ index.js
@@ -3,15 +3,19 @@
 
 
 
-
+   const ratingForm = document.getElementById('ratingForm');
    const ratingAnswer = document.getElementsByName('rating:checked').value;
    const ratingQuestion = document.getElementById('ratingQuestion');
    const thankYouBox = document.getElementById('thankYou');
    const ratingSelect = document.getElementById('chosenRating');
 
+
+   
+ 
+
  const ratingSheetAnswer = () => {
          ratingQuestion.style.visibility ="hidden";
-         thankYouBox.style.visibility ="";
-         ratingSelect.innerHTML="You selected ${ratingAnswer} out of 5"
+         thankYouBox.style.visibility ="visible";
+         ratingSelect.innerHTML=`You selected ${answer} out of 5`
       
    };
